fb2191ced2ff75b1372032202fc9fa8adf8ae263
Morten Lohne <lohnemorten@gmail.com>
2020-02-18 14:15:13 +0100

    Extract the boilerplate in mcts tactics tests into a separate function

--- src/tests/mcts_tests.rs
+++ src/tests/mcts_tests.rs
@@ -1,194 +1,87 @@
+use crate::tests::mcts_tests::TacticAnswer::PlayMoves;
 use crate::{board_mod, mcts};
 use board_game_traits::board::Board;
 use pgn_traits::pgn::PgnBoard;
 
 #[test]
 fn win_in_two_moves_test() {
-    let mut board = board_mod::Board::default();
-    let mut moves = vec![];
+    let move_strings = ["c3", "e5", "c2", "d5", "c1", "c5", "d3", "a4", "e3"];
 
-    for mv_san in ["c3", "e5", "c2", "d5", "c1", "c5", "d3", "a4", "e3"].iter() {
-        let mv = board.move_from_san(&mv_san).unwrap();
-        board.generate_moves(&mut moves);
-        assert!(moves.contains(&mv));
-        board.do_move(mv);
-        moves.clear();
-    }
-    let mut tree = mcts::Tree::new_root();
-    for _ in 0..10_000 {
-        tree.select(&mut board.clone());
-    }
-    let (mv, _score) = tree.best_move();
-    assert!(
-        mv == board.move_from_san("b4").unwrap()
-            || mv == board.move_from_san("b5").unwrap()
-            || mv == board.move_from_san("Cb4").unwrap()
-            || mv == board.move_from_san("Cb5").unwrap()
-    )
+    plays_correct_move_property(
+        &move_strings,
+        TacticAnswer::PlayMoves(&["b4", "b5", "Cb4", "Cb5"]),
+    );
 }
 
 #[test]
 fn black_win_in_one_move_test() {
-    let mut board = board_mod::Board::default();
-    let mut moves = vec![];
-
-    for mv_san in [
+    let move_strings = [
         "c2", "b4", "d2", "c4", "b2", "c3", "d3", "b3", "1c2-", "1b3>", "1d3<", "1c4+", "d4",
         "4c3<2", "c2", "c4", "1d4<", "1b4>", "d3", "b4", "b1", "d4", "1b2-", "2a3>", "e1", "5b3+3",
         "b3", "d1", "1e1<", "a5", "e1", "b5", "1b3-", "2c4<", "1e1-",
-    ]
-    .iter()
-    {
-        let mv = board.move_from_san(&mv_san).unwrap();
-        board.generate_moves(&mut moves);
-        assert!(
-            moves.contains(&mv),
-            "Move {} was not among legal moves: {:?}\n{:?}",
-            board.move_to_san(&mv),
-            moves,
-            board
-        );
-        board.do_move(mv);
-        moves.clear();
-    }
-    let (best_move, _score) = mcts::mcts(board.clone(), 20_000);
-    assert_eq!(
-        best_move,
-        board.move_from_san("3b4+").unwrap(),
-        "Black didn't win on board:\n{:?}",
-        board
-    );
+    ];
+
+    plays_correct_move_property(&move_strings, TacticAnswer::PlayMoves(&["3b4+"]));
 }
 
 #[test]
 fn white_can_win_in_one_move_test() {
-    let mut board = board_mod::Board::default();
-    let mut moves = vec![];
+    let move_strings = ["c2", "b4", "d2", "c4", "b2", "d4", "e2", "c3"];
 
-    for mv_san in ["c2", "b4", "d2", "c4", "b2", "d4", "e2", "c3"].iter() {
-        let mv = board.move_from_san(&mv_san).unwrap();
-        board.generate_moves(&mut moves);
-        assert!(
-            moves.contains(&mv),
-            "Move {} was not among legal moves: {:?}\n{:?}",
-            board.move_to_san(&mv),
-            moves,
-            board
-        );
-        board.do_move(mv);
-        moves.clear();
-    }
-    let (best_move, _score) = mcts::mcts(board.clone(), 90_000);
-    assert!(
-        best_move == board.move_from_san("a2").unwrap()
-            || best_move == board.move_from_san("Ca2").unwrap()
-    );
+    plays_correct_move_property(&move_strings, TacticAnswer::PlayMoves(&["a2", "Ca2"]));
 }
 
 #[test]
 fn black_avoid_loss_in_one_test() {
-    let mut board = board_mod::Board::default();
-    let mut moves = vec![];
+    let move_strings = ["c2", "b4", "d2", "c4", "b2", "d4", "e2"];
 
-    for mv_san in ["c2", "b4", "d2", "c4", "b2", "d4", "e2"].iter() {
-        let mv = board.move_from_san(&mv_san).unwrap();
-        board.generate_moves(&mut moves);
-        assert!(
-            moves.contains(&mv),
-            "Move {} was not among legal moves: {:?}\n{:?}",
-            board.move_to_san(&mv),
-            moves,
-            board
-        );
-        board.do_move(mv);
-        moves.clear();
-    }
-    let (best_move, _score) = mcts::mcts(board.clone(), 90_000);
-    assert!(
-        best_move == board.move_from_san("a2").unwrap()
-            || best_move == board.move_from_san("Sa2").unwrap()
-            || best_move == board.move_from_san("Ca2").unwrap(),
-        "Black didn't avoid loss on board:\n{:?}",
-        board
+    plays_correct_move_property(
+        &move_strings,
+        TacticAnswer::PlayMoves(&["a2", "Ca2", "Sa2"]),
     );
 }
 
 #[test]
 fn black_avoid_loss_in_one_test2() {
-    let mut board = board_mod::Board::default();
-    let mut moves = vec![];
-
-    for mv_san in [
+    let move_strings = [
         "c2", "b4", "d2", "d4", "b2", "c4", "e2", "a2", "c3", "b3", "1b2-", "1c4+", "1c2-", "b2",
         "b1", "d1", "c2", "a3", "2b3+", "1a2>", "1b1-",
-    ]
-    .iter()
-    {
-        let mv = board.move_from_san(&mv_san).unwrap();
-        board.generate_moves(&mut moves);
-        assert!(
-            moves.contains(&mv),
-            "Move {} was not among legal moves: {:?}\n{:?}",
-            board.move_to_san(&mv),
-            moves,
-            board
-        );
-        board.do_move(mv);
-        moves.clear();
-    }
-    let (best_move, _score) = mcts::mcts(board.clone(), 30_000);
-    assert_eq!(
-        best_move,
-        board.move_from_san("1d1-").unwrap(),
-        "Black didn't avoid loss on board:\n{:?}",
-        board
-    );
+    ];
+    plays_correct_move_property(&move_strings, TacticAnswer::PlayMoves(&["1d1-"]));
 }
 
 #[test]
 fn black_avoid_loss_in_one_test3() {
-    let mut board = board_mod::Board::default();
-    let mut moves = vec![];
-
-    for mv_san in [
+    let move_strings = [
         "c2", "c3", "d2", "d3", "1d2-", "c4", "d2", "b4", "1c2-", "1c4+", "2d3<", "d4", "b2", "a5",
-        "c2", "a2", "b1", "1a2>", "1b1-", "1d4+", "5c3>3", "c3",
-    ]
-    .iter()
-    {
-        let mv = board.move_from_san(&mv_san).unwrap();
-        board.generate_moves(&mut moves);
-        assert!(
-            moves.contains(&mv),
-            "Move {} was not among legal moves: {:?}\n{:?}",
-            board.move_to_san(&mv),
-            moves,
-            board
-        );
-        board.do_move(mv);
-        moves.clear();
-    }
-    let (best_move, _score) = mcts::mcts(board.clone(), 30_000);
-    assert!(
-        best_move == board.move_from_san("Ca2").unwrap()
-            || best_move == board.move_from_san("Sa2").unwrap(),
-        "Black didn't avoid loss on board:\n{:?}",
-        board
-    );
+        "c2", "a2", "b1", "1a2>", "1b1-", "1d4+", "5c3>3",
+    ];
+
+    plays_correct_move_property(&move_strings, TacticAnswer::PlayMoves(&["Ca2", "Sa2"]));
 }
 
 #[test]
 fn black_avoid_loss_in_one_test4() {
-    let mut board = board_mod::Board::default();
-    let mut moves = vec![];
-
-    for mv_san in [
+    let moves_strings = [
         "c2", "c3", "d2", "b4", "1c2-", "d4", "c2", "b2", "1c2<", "d5", "c2", "a3", "e2", "a2",
         "b1", "a1", "d3", "c4", "2c3-", "1d4<", "Cc3", "3c4>2", "1c3-", "e1", "c3", "1a1>", "d1",
         "c5", "b5", "1b4-", "1d3-",
-    ]
-    .iter()
-    {
+    ];
+
+    plays_correct_move_property(&moves_strings, TacticAnswer::AvoidMoves(&["2b5>1"]));
+}
+
+/// The correct answer to a tactic can be either a lost of winning/non-losing moves, or simply a list of moves to specifically avoid
+enum TacticAnswer {
+    AvoidMoves(&'static [&'static str]),
+    PlayMoves(&'static [&'static str]),
+}
+
+fn plays_correct_move_property(move_strings: &[&str], correct_moves: TacticAnswer) {
+    let mut board = board_mod::Board::default();
+    let mut moves = vec![];
+
+    for mv_san in move_strings.iter() {
         let mv = board.move_from_san(&mv_san).unwrap();
         board.generate_moves(&mut moves);
         assert!(
@@ -201,9 +94,27 @@ fn black_avoid_loss_in_one_test4() {
         board.do_move(mv);
         moves.clear();
     }
-    let (best_move, _score) = mcts::mcts(board.clone(), 20_000);
-    assert_ne!(best_move, board.move_from_san("2b5>1").unwrap(),
-        "Black didn't avoid loss on board:\n{:?}",
-        board
-    );
+
+    let mut mcts = mcts::Tree::new_root();
+
+    for i in 1..50000 {
+        mcts.select(&mut board.clone());
+        if i % 10000 == 0 {
+            let (best_move, _score) = mcts.best_move();
+            match correct_moves {
+                TacticAnswer::AvoidMoves(moves) =>
+                    assert!(moves
+                        .iter()
+                        .all(|mv| best_move != board.move_from_san(mv).unwrap()),
+                            "{} played {}, one of the losing moves {:?} after {} iterations on board:\n{:?}",
+                            board.side_to_move(), board.move_to_san(&best_move), moves, i, board),
+                TacticAnswer::PlayMoves(moves) =>
+                    assert!(moves
+                                .iter()
+                                .any(|mv| best_move == board.move_from_san(mv).unwrap()),
+                            "{} didn't play one of {:?} to avoid loss, {} played instead after {} iterations on board:\n{:?}",
+                            board.side_to_move(), moves, board.move_to_san(&best_move), i, board),
+            }
+        }
+    }
 }
