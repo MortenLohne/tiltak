b9485b855a4cb2b0db93a71b464264fb8e5f6f2c
Morten Lohne <lohnemorten@gmail.com>
2020-02-20 22:58:15 +0100

    Decide best move based on number of visits

--- src/mcts.rs
+++ src/mcts.rs
@@ -51,7 +51,8 @@ impl Tree {
 
     pub fn best_move(&self) -> (Move, f64) {
         let mut best_children = self.children.clone();
-        best_children.sort_by_key(|(child, _)| (child.mean_action_value * 1000_000.0) as u64);
+        best_children.sort_by_key(|(child, _)| child.visits);
+        best_children.reverse();
         (
             best_children[0].1.clone(),
             best_children[0].0.mean_action_value,
