5e113c25b6bce7719fbf1ed88e8927aa6fcfd9ff
Morten Lohne <lohnemorten@gmail.com>
2020-02-20 22:32:20 +0100

    Add more mcts tests

--- src/tests/mcts_tests.rs
+++ src/tests/mcts_tests.rs
@@ -85,6 +85,36 @@ fn black_avoid_less_in_one_test5() {
 }
 
 #[test]
+fn white_avoid_loss_in_one_test() {
+    let move_strings = [
+        "c3", "c4", "b4", "1c4+", "d2", "b5", "b3", "1b5+", "1b3>", "d4", "2c3+", "c4", "d3",
+        "1d4+", "d4", "1c4+", "b2", "c4", "1d4+", "2c3>", "1d2-", "Sb3", "5d3+3", "1b3+", "d4",
+        "2b2>1", "3c2-1", "b3", "b2", "1b3+", "c2", "b3", "c5", "2b2>", "b2", "1b3+", "b3", "2b4+",
+        "d5", "b4", "2c4<", "3b3-", "2c3+", "2b2>", "3d1<", "3c2+", "d1", "5b4+4",
+    ];
+
+    plays_correct_move_property(
+        &move_strings,
+        TacticAnswer::PlayMoves(&["Cb5", "Sb5", "b5", "1c5<", "1d1<"]),
+    );
+}
+
+#[test]
+fn white_avoid_loss_in_one_test2() {
+    let move_strings = [
+        "c3", "c4", "b3", "b4", "d3", "1b4+", "b2", "b4", "d4", "1c4+", "d2", "c4", "c2", "2b3+",
+        "1c2-", "1c4+", "1d3<", "d5", "4c3>2", "1d5+", "2d3-", "Sc4", "3d4+", "1c4+", "c4", "2c3>",
+        "1c4<", "5d3-", "2b4+1", "1b3+", "Se1", "4d4<3", "c3", "5b2>2", "1c3+", "3d2<", "b3",
+        "3b4+", "b4", "1c4<", "d3", "c4", "1d3-", "1c4>", "e4", "2d4>", "2e3-", "2d4>", "d3", "a4",
+    ];
+
+    plays_correct_move_property(
+        &move_strings,
+        TacticAnswer::PlayMoves(&["Cd4", "Sd4", "Cc4", "Sc4"]),
+    );
+}
+
+#[test]
 fn do_not_suicide_as_black_test() {
     let move_strings = [
         "c2", "c4", "d2", "c3", "b2", "d3", "1d2-", "b3", "d2", "b4", "1c2-", "1b3>", "2d3<",
@@ -132,6 +162,7 @@ fn plays_correct_move_property(move_strings: &[&str], correct_moves: TacticAnswe
         moves.clear();
     }
 
+    board.generate_moves(&mut moves);
     let mut mcts = mcts::Tree::new_root();
 
     for i in 1..50000 {
