f0913759be9cfc00e50f5877916f459352f013b6
Morten Lohne <lohnemorten@gmail.com>
2020-03-09 23:55:17 +0100

    Add crate-level code examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! A library implementing the rules for Tak, including a fairly strong AI.
+//!
+//! # Examples
+//!
+//! Generate legal moves for the start position
+//!
+//! ```
+//! use taik::board::Board;
+//! use board_game_traits::board::Board as BoardTrait;
+//!
+//! let board = Board::start_board();
+//! let mut moves = vec![];
+//! board.generate_moves(&mut moves);
+//! assert_eq!(moves.len(), 25);
+//! ```
+//!
+//! Run Monte Carlo Tree Search for the start position
+//!
+//! ```rust,no_run
+//! use taik::board::Board;
+//! use taik::mcts;
+//!
+//! let board = Board::default();
+//! let (best_move, score) = mcts(board, 100_000);
+//! println!("Played {} with score {}", best_move, score);
+//! ```
+
 extern crate arrayvec;
 extern crate board_game_traits;
 extern crate pgn_traits;
