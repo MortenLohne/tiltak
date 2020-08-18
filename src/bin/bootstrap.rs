a46658e12a306ddcea0f8c0d4712315452108353
Morten Lohne <lohnemorten@gmail.com>
2020-08-18 23:48:38 +0200

    Rename aws lambda server binary to bootstrap

--- src/bin/bootstrap.rs
+++ src/bin/bootstrap.rs
@@ -1,15 +1,15 @@
 use board_game_traits::board::Board as EvalBoard;
 use lambda_runtime::{error::HandlerError, lambda, Context};
 use serde::{Deserialize, Serialize};
-use std::time;
+use std::time::Duration;
 use taik::board::{Board, Move};
 use taik::mcts;
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 struct Event {
     moves: Vec<Move>,
-    time_left: time::Duration,
-    increment: time::Duration,
+    time_left: Duration,
+    increment: Duration,
 }
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
@@ -27,7 +27,10 @@ fn event_handler(e: Event, _c: Context) -> Result<Output, HandlerError> {
     for mv in e.moves {
         board.do_move(mv);
     }
-    let (best_move, score) = mcts::play_move_time(board, e.time_left / 40 + e.increment);
+
+    let max_time = Duration::min(e.time_left / 40 + e.increment, Duration::from_secs(60));
+
+    let (best_move, score) = mcts::play_move_time(board, max_time);
 
     Ok(Output { best_move, score })
 }
