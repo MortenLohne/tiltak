8903aeb98e65143ed4cdc84b4627e660908d45ad
Morten Lohne <lohnemorten@gmail.com>
2021-01-01 23:24:47 +0100

    Rename functions to javascript naming conventions

--- src/search/wasm.rs
+++ src/search/wasm.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use crate::board::{Board, Move};
 use crate::search::MonteCarloTree;
 use board_game_traits::board::{Board as BoardTrait, Color};
@@ -7,12 +9,19 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 impl MonteCarloTree {
-    pub fn search_iteration(&mut self) {
-        self.select();
+    /// Run `n` iterations of MCTS. Subsequent calls to this function will continue the same search, not restart it.
+    pub fn doSearchIterations(&mut self, n: u32) {
+        for _ in 0..n {
+            self.select();
+        }
     }
 
-    pub fn best_move2(&self) -> Option<String> {
-        if self.visits() < 2 {
+    /// Returns the best move in the position, as determined by the search so far. Will return `undefined` if no calls to `doSearchIterations` have been done, or if the game is already decided.
+    pub fn bestMove(&self) -> Option<String> {
+        if self.visits() < 2
+            || self.edge.child.is_none()
+            || self.edge.child.as_ref().unwrap().is_terminal
+        {
             None
         } else {
             Some(self.best_move().0.to_string())
@@ -21,21 +30,21 @@ impl MonteCarloTree {
 }
 #[wasm_bindgen]
 impl Board {
-    pub fn do_move2(&mut self, move_string: &str) -> JsValue {
+    pub fn doMove(&mut self, move_string: &str) -> JsValue {
         let reverse_move = self.do_move(Move::from_str(move_string).unwrap());
         JsValue::from_serde(&reverse_move).unwrap()
     }
 
-    pub fn reverse_move2(&mut self, reverse_move_string: JsValue) {
+    pub fn reverseMove(&mut self, reverse_move_string: JsValue) {
         let reverse_move = JsValue::into_serde(&reverse_move_string).unwrap();
         self.reverse_move(reverse_move);
     }
 
-    pub fn start_board2() -> Self {
+    pub fn startBoard() -> Self {
         Self::start_board()
     }
 
-    pub fn legal_moves(&self) -> js_sys::Array {
+    pub fn legalMoves(&self) -> js_sys::Array {
         let mut moves = vec![];
         self.generate_moves(&mut moves);
         moves
@@ -44,18 +53,18 @@ impl Board {
             .collect()
     }
 
-    pub fn side_to_move2(&self) -> u32 {
+    pub fn sideToMove(&self) -> u32 {
         match self.side_to_move() {
             Color::White => 1,
             Color::Black => 2,
         }
     }
 
-    pub fn game_is_over(&self) -> bool {
+    pub fn gameIsOver(&self) -> bool {
         self.game_result().is_some()
     }
 
-    pub fn from_tps(tps_string: &str) -> Result<Board, JsValue> {
+    pub fn fromTps(tps_string: &str) -> Result<Board, JsValue> {
         Self::from_fen(tps_string).map_err(|err| JsValue::from_serde(&err.to_string()).unwrap())
     }
 }
