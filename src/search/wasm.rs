319821251db7530b0c55fddb85c67d0303d8a886
Morten Lohne <lohnemorten@gmail.com>
2021-01-01 21:48:33 +0100

    Use FromStr implemtation of Move

--- src/search/wasm.rs
+++ src/search/wasm.rs
@@ -1,7 +1,7 @@
-use crate::board::Board;
+use crate::board::{Board, Move};
 use crate::search::MonteCarloTree;
 use board_game_traits::board::Board as BoardTrait;
-use pgn_traits::pgn::PgnBoard;
+use std::str::FromStr;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -19,9 +19,8 @@ impl MonteCarloTree {
     }
 }
 impl Board {
-    pub fn do_move2(&mut self, move_string: String) -> JsValue {
-        let dummy_board = Board::start_board();
-        let reverse_move = self.do_move(dummy_board.move_from_san(&move_string).unwrap());
+    pub fn do_move2(&mut self, move_string: &str) -> JsValue {
+        let reverse_move = self.do_move(Move::from_str(move_string).unwrap());
         JsValue::from_serde(&reverse_move).unwrap()
     }
 }
