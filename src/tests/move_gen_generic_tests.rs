a0de23b391ad5cd432b5bbb57def8a9727bf0124
Morten Lohne <lohnemorten@gmail.com>
2021-01-29 00:22:33 +0100

    Fix start board move gen test

--- src/tests/move_gen_generic_tests.rs
+++ src/tests/move_gen_generic_tests.rs
@@ -3,7 +3,6 @@ use board_game_traits::board::Board as EvalBoard;
 
 #[test]
 fn start_board_move_gen_test() {
-    start_board_move_gen_prop::<3>();
     start_board_move_gen_prop::<4>();
     start_board_move_gen_prop::<5>();
     start_board_move_gen_prop::<6>();
@@ -90,12 +89,12 @@ fn start_board_move_gen_prop<const S: usize>() {
     let mut board = <Board<S>>::default();
     let mut moves = vec![];
     board.generate_moves(&mut moves);
-    assert_eq!(moves.len(), 25);
+    assert_eq!(moves.len(), S * S);
     for mv in moves {
         let reverse_move = board.do_move(mv);
         let mut moves = vec![];
         board.generate_moves(&mut moves);
-        assert_eq!(moves.len(), 24);
+        assert_eq!(moves.len(), S * S - 1);
         board.reverse_move(reverse_move);
     }
 }
