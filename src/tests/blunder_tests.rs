ab1cc480b66c299b464f2245ff42a926b5a29f81
Morten Lohne <lohnemorten@gmail.com>
2021-07-05 19:56:45 +0200

    Add first blunder test

--- src/tests/blunder_tests.rs
+++ src/tests/blunder_tests.rs
@@ -1,41 +1,22 @@
-mod blunder_tests;
-mod board_generic_tests;
-mod board_tests;
-mod mcts_tests;
-mod move_gen_5s_tests;
-mod move_gen_generic_tests;
-mod ptn_tests;
-mod tactics_tests_5s;
-mod tactics_tests_6s;
+//! Test that we don't make one-move blunders on very low node counts
 
 use crate::position::Position;
 use crate::search;
 use board_game_traits::Position as PositionTrait;
 use pgn_traits::PgnPosition;
 
-fn do_moves_and_check_validity<const S: usize>(position: &mut Position<S>, move_strings: &[&str]) {
-    let mut moves = vec![];
-    for mv_san in move_strings.iter() {
-        let mv = position.move_from_san(&mv_san).unwrap();
-        position.generate_moves(&mut moves);
-        assert!(
-            moves.contains(&mv),
-            "Move {} was not among legal moves: {:?}\n{:?}",
-            position.move_to_san(&mv),
-            moves,
-            position
-        );
-        position.do_move(mv);
-        moves.clear();
-    }
+#[test]
+fn do_not_blunder_road_win() {
+    let position: Position<6> = Position::from_fen("x2,2,1,x2/x,2,22,21S,2,2/x,22221C,2,221111211112C,1221S,x/2,2,2S,1S,1,1/x,2,1,2,1,12/1,2,1,21,221,2 2 44").unwrap();
+
+    do_not_blunder_property(
+        position,
+        &["f1<", "d2>", "2f2<", "b1<", "2f2+", "d2-", "b1>", "f2<"],
+    )
 }
 
-fn plays_correct_hard_move_property<const S: usize>(move_strings: &[&str], correct_moves: &[&str]) {
-    let mut position = <Position<S>>::default();
+fn do_not_blunder_property<const S: usize>(position: Position<S>, correct_moves: &[&str]) {
     let mut moves = vec![];
-
-    do_moves_and_check_validity(&mut position, move_strings);
-
     position.generate_moves(&mut moves);
 
     for move_string in correct_moves {
@@ -51,7 +32,7 @@ fn plays_correct_hard_move_property<const S: usize>(move_strings: &[&str], corre
             position
         );
     }
-    let (best_move, score) = search::mcts(position.clone(), 50_000);
+    let (best_move, score) = search::mcts(position.clone(), 10_000);
 
     assert!(
         correct_moves
