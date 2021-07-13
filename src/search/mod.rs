e40564ac8ba024dbf62b6ceaf76a17e12833e5c3
Morten Lohne <lohnemorten@gmail.com>
2021-07-13 13:05:53 +0200

    fmt

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -32,7 +32,7 @@ impl<const S: usize> Default for MctsSetting<S> {
         MctsSetting {
             value_params: Vec::from(<Position<S>>::value_params()),
             policy_params: Vec::from(<Position<S>>::policy_params()),
-            search_params: vec![1.1, 2800.0, -0.05, 0.9],
+            search_params: vec![1.1, 2800.0, -0.05, 0.8],
             dirichlet: None,
             excluded_moves: vec![],
             rollout_depth: 0,
