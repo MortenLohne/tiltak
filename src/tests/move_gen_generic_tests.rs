0675e7a1e731b9d783df01b2ea74d24ae4439e96
alion02 <alojczek@gmail.com>
2022-02-12 22:57:53 +0100

    Move `perft` to `Position`

--- src/tests/move_gen_generic_tests.rs
+++ src/tests/move_gen_generic_tests.rs
@@ -10,78 +10,17 @@ fn start_position_move_gen_test() {
     start_position_move_gen_prop::<8>();
 }
 
-pub fn perft<const S: usize>(position: &mut Position<S>, depth: u16) -> u64 {
-    if depth == 0 || position.game_result().is_some() {
-        1
-    } else {
-        let mut moves = vec![];
-        position.generate_moves(&mut moves);
-        moves
-            .into_iter()
-            .map(|mv| {
-                let old_position = position.clone();
-                let reverse_move = position.do_move(mv.clone());
-                let num_moves = perft(position, depth - 1);
-                position.reverse_move(reverse_move);
-                debug_assert_eq!(
-                    *position, old_position,
-                    "Failed to restore old board after {:?} on\n{:?}",
-                    mv, old_position
-                );
-                num_moves
-            })
-            .sum()
-    }
-}
-
 /// Verifies the perft result of a position against a known answer
 pub fn perft_check_answers<const S: usize>(position: &mut Position<S>, answers: &[u64]) {
     for (depth, &answer) in answers.iter().enumerate() {
-        assert_eq!(
-            perft(position, depth as u16),
-            answer,
-            "Wrong perft result on\n{:?}",
-            position
-        );
-        assert_eq!(
-            perft(&mut position.flip_board_x(), depth as u16),
-            answer,
-            "Wrong perft result on\n{:?}",
-            position
-        );
-        assert_eq!(
-            perft(&mut position.flip_board_y(), depth as u16),
-            answer,
-            "Wrong perft result on\n{:?}",
-            position
-        );
-        assert_eq!(
-            perft(&mut position.flip_colors(), depth as u16),
-            answer,
-            "Wrong perft result on\n{:?}",
-            position
-        );
-        assert_eq!(
-            perft(&mut position.rotate_board(), depth as u16),
-            answer,
-            "Wrong perft result on\n{:?}",
-            position
-        );
-        assert_eq!(
-            perft(&mut position.rotate_board().rotate_board(), depth as u16),
-            answer,
-            "Wrong perft result on\n{:?}",
-            position
-        );
-        assert_eq!(
-            perft(
-                &mut position.rotate_board().rotate_board().rotate_board(),
-                depth as u16
-            ),
-            answer,
-            "Wrong perft result on\n{:?}",
-            position
-        );
+        for mut position in position.symmetries_with_swapped_colors() {
+            assert_eq!(
+                position.perft(depth as u16),
+                answer,
+                "Wrong perft result on\n{:?}",
+                position
+            );
+        }
     }
 }
 
