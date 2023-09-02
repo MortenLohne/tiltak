8600dc0b08f21e3caf40dc33f6b6ac1107224523
Morten Lohne <lohnemorten@gmail.com>
2023-09-02 22:43:41 +0200

    Add failing 5s tactics test

--- src/tests/tactics_tests_5s.rs
+++ src/tests/tactics_tests_5s.rs
@@ -183,3 +183,13 @@ fn double_tak_threat_from_citadel_test() {
     };
     test_position.plays_correct_move_long_prop::<5>(&["e2<"]);
 }
+
+#[test]
+fn impure_spread_onto_string_line() {
+    let test_position = TestPosition {
+        tps_string: Some("2,2,x2,1/x,2,x2,21/2,221C,22121,x,121/12C,x2,112,1/x2,1,2221S,2S 1 27"),
+        move_strings: &[],
+        komi: Komi::default(),
+    };
+    test_position.plays_correct_move_long_prop::<5>(&["3b3>111"]);
+}
