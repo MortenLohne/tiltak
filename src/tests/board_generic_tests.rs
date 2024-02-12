1ddc69b9acbf781d953c1797aca909f13880dd54
Morten Lohne <lohnemorten@gmail.com>
2024-02-12 22:42:09 +0100

    Run certain size-generic tests for 7s and 8s

--- src/tests/board_generic_tests.rs
+++ src/tests/board_generic_tests.rs
@@ -1,8 +1,11 @@
 use crate::evaluation::parameters::IncrementalPolicy;
 use crate::evaluation::parameters::PolicyApplier;
+use crate::position::starting_capstones;
+use crate::position::Komi;
 use board_game_traits::{Color, EvalPosition, GameResult::*, Position as PositionTrait};
 use pgn_traits::PgnPosition;
 use rand::seq::SliceRandom;
+use rand::Rng;
 
 use crate::position::{squares_iterator, Role, Square};
 use crate::position::{GroupData, Move};
@@ -22,6 +25,36 @@ fn play_random_5s_games_test() {
 #[test]
 fn play_random_6s_games_test() {
     play_random_games_prop::<6>(200)
+}
+
+#[test]
+fn play_random_3s_games_no_eval_test() {
+    play_random_games_no_eval_prop::<3>(1000)
+}
+
+#[test]
+fn play_random_4s_games_no_eval_test() {
+    play_random_games_no_eval_prop::<4>(1000)
+}
+
+#[test]
+fn play_random_5s_games_no_eval_test() {
+    play_random_games_no_eval_prop::<5>(1000)
+}
+
+#[test]
+fn play_random_6s_games_no_eval_test() {
+    play_random_games_no_eval_prop::<6>(1000)
+}
+
+#[test]
+fn play_random_7s_games_no_eval_test() {
+    play_random_games_no_eval_prop::<7>(1000)
+}
+
+#[test]
+fn play_random_8s_games_no_eval_test() {
+    play_random_games_no_eval_prop::<8>(1000)
 }
 
 #[test]
@@ -50,7 +83,8 @@ fn play_random_games_prop<const S: usize>(num_games: usize) {
 
     let mut rng = rand::thread_rng();
     for _ in 0..num_games {
-        let mut position = <Position<S>>::default();
+        let komi = Komi::from_half_komi(if rng.gen() { 0 } else { 4 }).unwrap();
+        let mut position = <Position<S>>::start_position_with_komi(komi);
         let mut moves = vec![];
 
         for i in 0.. {
@@ -64,13 +98,17 @@ fn play_random_games_prop<const S: usize>(num_games: usize) {
             );
             assert_eq!(position, position.flip_colors().flip_colors());
 
-            assert_eq!(Position::from_fen(&position.to_fen()).unwrap(), position);
+            assert_eq!(
+                Position::from_fen_with_komi(&position.to_fen(), komi).unwrap(),
+                position
+            );
 
             let group_data: GroupData<S> = position.group_data();
 
             assert!((group_data.white_road_pieces() & group_data.black_road_pieces()).is_empty());
             assert!(
-                (group_data.white_road_pieces() & group_data.white_blocking_pieces()).count() <= 1
+                (group_data.white_road_pieces() & group_data.white_blocking_pieces()).count()
+                    <= starting_capstones(S)
             );
 
             let eval = position.static_eval();
@@ -187,9 +225,116 @@ fn play_random_games_prop<const S: usize>(num_games: usize) {
         }
     }
     println!(
-        "{} white wins, {} black wins, {} draws, {} moves played.",
-        white_wins, black_wins, draws, duration
+        "{}s: {} white wins, {} black wins, {} draws, {} moves played.",
+        S, white_wins, black_wins, draws, duration
     )
+}
+
+fn play_random_games_no_eval_prop<const S: usize>(num_games: usize) {
+    let mut white_wins = 0;
+    let mut black_wins = 0;
+    let mut draws = 0;
+    let mut duration = 0;
+
+    let mut rng = rand::thread_rng();
+    for _ in 0..num_games {
+        let komi = Komi::from_half_komi(rng.gen_range(-6..=6)).unwrap();
+        let mut position = <Position<S>>::start_position_with_komi(komi);
+        let mut moves = vec![];
+
+        for i in 0.. {
+            let hash_from_scratch = position.zobrist_hash_from_scratch();
+            assert_eq!(
+                hash_from_scratch,
+                position.zobrist_hash(),
+                "Hash mismatch for board:\n{:?}\nMoves: {:?}",
+                position,
+                position.moves()
+            );
+            assert_eq!(position, position.flip_colors().flip_colors());
+
+            assert_eq!(
+                Position::from_fen_with_komi(&position.to_fen(), komi).unwrap(),
+                position
+            );
+
+            let group_data: GroupData<S> = position.group_data();
+
+            assert!((group_data.white_road_pieces() & group_data.black_road_pieces()).is_empty());
+            assert!(
+                (group_data.white_road_pieces() & group_data.white_blocking_pieces()).count()
+                    <= starting_capstones(S)
+            );
+
+            moves.clear();
+
+            position.generate_moves(&mut moves);
+
+            for mv in moves.iter() {
+                assert_eq!(*mv, Move::compress(mv.expand()));
+            }
+
+            let mv = moves
+                .choose(&mut rng)
+                .unwrap_or_else(|| panic!("No legal moves on board\n{:?}", position));
+            assert_eq!(
+                *mv,
+                position.move_from_san(&position.move_to_san(mv)).unwrap()
+            );
+
+            let white_flat_lead_before = group_data.white_flat_stones.count() as i8
+                - group_data.black_flat_stones.count() as i8;
+
+            let fcd = position.fcd_for_move(*mv);
+
+            position.do_move(*mv);
+
+            let new_group_data = position.group_data();
+
+            let white_flat_lead_after = new_group_data.white_flat_stones.count() as i8
+                - new_group_data.black_flat_stones.count() as i8;
+
+            match !position.side_to_move() {
+                Color::White => assert_eq!(white_flat_lead_after, white_flat_lead_before + fcd),
+                Color::Black => assert_eq!(white_flat_lead_after, white_flat_lead_before - fcd),
+            }
+
+            assert_ne!(hash_from_scratch, position.zobrist_hash_from_scratch());
+
+            let result = position.game_result();
+            for rotation in position.symmetries() {
+                assert_eq!(rotation.game_result(), result);
+            }
+
+            match result {
+                None => (),
+                Some(WhiteWin) => {
+                    white_wins += 1;
+                    duration += i;
+                    break;
+                }
+                Some(BlackWin) => {
+                    black_wins += 1;
+                    duration += i;
+                    break;
+                }
+                Some(Draw) => {
+                    draws += 1;
+                    duration += i;
+                    break;
+                }
+            }
+        }
+    }
+    println!(
+        "{}s: {} white wins, {} black wins, {} draws, {} moves played.",
+        S, white_wins, black_wins, draws, duration
+    )
+}
+
+#[test]
+fn go_in_directions_3s_test() {
+    go_in_directions_prop::<3>()
 }
 
 #[test]
@@ -207,6 +352,16 @@ fn go_in_directions_6s_test() {
     go_in_directions_prop::<6>()
 }
 
+#[test]
+fn go_in_directions_7s_test() {
+    go_in_directions_prop::<7>()
+}
+
+#[test]
+fn go_in_directions_8s_test() {
+    go_in_directions_prop::<8>()
+}
+
 fn go_in_directions_prop<const S: usize>() {
     for square in squares_iterator::<S>() {
         assert_eq!(square.directions().count(), square.neighbors().count());
@@ -219,6 +374,11 @@ fn go_in_directions_prop<const S: usize>() {
             )
         }
     }
+}
+
+#[test]
+fn group_connection_3s_test() {
+    group_connection_generic_prop::<3>()
 }
 
 #[test]
@@ -235,6 +395,15 @@ fn group_connection_5s_test() {
 fn group_connection_6s_test() {
     group_connection_generic_prop::<6>()
 }
+#[test]
+fn group_connection_7s_test() {
+    group_connection_generic_prop::<7>()
+}
+#[test]
+
+fn group_connection_8s_test() {
+    group_connection_generic_prop::<8>()
+}
 
 fn group_connection_generic_prop<const S: usize>() {
     let group_connection = GroupEdgeConnection::default();
@@ -246,6 +415,11 @@ fn group_connection_generic_prop<const S: usize>() {
     assert!(!a1_connection.is_connected_east());
     assert!(a1_connection.is_connected_north());
     assert!(a1_connection.is_connected_west());
+}
+
+#[test]
+fn bitboard_full_board_file_rank_3s_test() {
+    bitboard_full_board_file_rank_prop::<3>()
 }
 
 #[test]
@@ -261,6 +435,16 @@ fn bitboard_full_board_file_rank_5s_test() {
 #[test]
 fn bitboard_full_board_file_rank_6s_test() {
     bitboard_full_board_file_rank_prop::<6>()
+}
+
+#[test]
+fn bitboard_full_board_file_rank_7s_test() {
+    bitboard_full_board_file_rank_prop::<7>()
+}
+
+#[test]
+fn bitboard_full_board_file_rank_8s_test() {
+    bitboard_full_board_file_rank_prop::<8>()
 }
 
 fn bitboard_full_board_file_rank_prop<const S: usize>() {
@@ -292,6 +476,11 @@ fn bitboard_full_board_file_rank_prop<const S: usize>() {
             }
         }
     }
+}
+
+#[test]
+fn square_rank_file_3s_test() {
+    square_rank_file_prop::<3>();
 }
 
 #[test]
